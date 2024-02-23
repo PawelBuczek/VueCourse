c90b396a7d031eacb4feb1b67b85bd0b98153992
PawelBuczek <pawel.buczek@email.com>
2024-02-23 09:07:51 +0100

    added first vue method

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
             courseGoal: 'Master Vue',
             vueLink: 'https://vuejs.org/'
         };
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return 'Finish Course';
+            } else {
+                return 'Learn Vue';
+            }
+        }
     }
 });
 
